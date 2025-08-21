08963e8ac3846f6e3dc22eb296684e4d7a065ba4
julio.cesar <juliocesar13b@gmail.com>
2025-08-21 13:14:43 -0300

    Criando testConfig

--- User/src/main/java/com/microservice/User/Config/TestConfig.java
+++ User/src/main/java/com/microservice/User/Config/TestConfig.java
@@ -1,4 +1,37 @@
 package com.microservice.User.Config;
 
-public class TestConfig {
+
+import com.microservice.User.models.entities.User;
+import com.microservice.User.repository.UserRepository;
+import com.microservice.User.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
+
+@Configuration
+
+public class TestConfig  implements CommandLineRunner {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+
+
+
+        userRepository.deleteAll();
+
+
+        User user1 = new User(null, "Julio", "123456");
+        User user2 = new User(null, "Maria", "senhaSegura");
+        User user3 = new User(null, "Carlos", "abc123");
+        User user4 = new User(null, "Ana", "teste@2025");
+
+
+        userRepository.saveAll(Arrays.asList(user1,user2,user3,user4));
+    }
 }
